89cffcd3e893046adb4bc6bf9945b6716d28c56b
KETHA-DIVYA-SREE <65858609+KETHA-DIVYA-SREE@users.noreply.github.com>
2022-09-09 01:53:07 +0530

    1 file modified

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require(`express`);
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
 app.listen(5000, () => {
-  console.log('server is listening at 5000');
+  console.log('server is started at 5000');
 });
